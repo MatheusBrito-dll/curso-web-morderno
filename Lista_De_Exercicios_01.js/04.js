84509f212ed8ae2543f42e3b19c6abfc9a67909f
Matheus Santos Brito <matheusbrito0253@gmail.com>
2023-03-30 17:53:35 -0400

    Exercicios

---
+++ Lista_De_Exercicios_01.js/04.js
@@ -0,0 +1,10 @@
+/**
+ * 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. 
+ * A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+ */
+
+function div_01(dividendo, divisor) {
+    return console.log(`Divisão = ${dividendo / divisor} Resto = ${dividendo % divisor}.`)
+}
+
+div_01(5, 2)
