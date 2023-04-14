1034eac28d263b367c3589771e66e2c9b964642b
Matheus Santos Brito <matheusbrito0253@gmail.com>
2023-04-14 15:28:47 -0400

    classe

---
+++ Objeto/Classe/classe_01.js
@@ -0,0 +1,36 @@
+class Lancamento {
+    constructor(nome = 'Gene', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes
+        this.ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos){ 
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+    
+    sumario() {
+      let valorConsolidado = 0
+      this.lancamentos.forEach(l => {
+        valorConsolidado += l.valor
+      })
+      return valorConsolidado  
+    }
+}
+
+const salario = new Lancamento('Salario', 45000)
+const contaDeluz = new Lancamento('Luz', -220)
+
+const contas = new CicloFinanceiro(6, 2018)
+
+contas.addLancamentos(salario, contaDeluz)
+console.log(contas.sumario())
+
+
