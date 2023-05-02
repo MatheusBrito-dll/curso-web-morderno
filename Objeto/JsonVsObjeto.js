02e29217020b24304d604dc1aca8f0b96358c2b3
Matheus Santos Brito <102887875+MatheusBrito-dll@users.noreply.github.com>
2023-05-01 21:19:58 -0300

    json

--- Objeto/JsonVsObjeto.js
+++ Objeto/JsonVsObjeto.js
@@ -1,6 +1,13 @@
-const obj = {a:1, b: 2, c:3, soma(){return a + b + c}}
-
+const obj = {
+    a:1, 
+    b:2, 
+    c:3, 
+    soma(){
+        return a + b + c
+    }
+}
+//OBJETO PARA JSON
 console.log(JSON.stringify(obj))
-
+//JSON PARA OBJETO
 console.log(JSON.parse('{"a": 1,"b": 2.1,"c": true}'))
 console.log(JSON.parse('{"Texto": "string","Objeto": {"Dentro": 5},"Array": []}'))
