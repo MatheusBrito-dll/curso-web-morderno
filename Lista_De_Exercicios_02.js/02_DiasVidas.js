11b37544d3bc145d9fa233d3391d22ec863f0b7f
Matheus Santos Brito <102887875+MatheusBrito-dll@users.noreply.github.com>
2023-05-02 22:07:22 -0300

    Ex 02

---
+++ Lista_De_Exercicios_02.js/02_DiasVidas.js
@@ -0,0 +1,11 @@
+/**
+ * Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+ * Obs: considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e desconsidere
+ * também dias decorridos desde o último aniversário.
+ */
+
+function aniversario(idade) {
+    return console.log(idade * 365)
+}
+aniversario(25)
+aniversario(70)
