ac7eeefb283ab79986d675c0c924df03607c2f9e
Matheus Santos Brito <102887875+MatheusBrito-dll@users.noreply.github.com>
2023-05-09 00:33:49 -0300

    operadores

---
+++ OperadorTernario/Operador.js
@@ -0,0 +1,12 @@
+const array = [];
+const mensagem = array.length ? "O array não está vazio" : "O array está vazio";
+console.log(mensagem); // "O array está vazio"
+
+
+const isLoggedIn = true;
+const mensagem2 = isLoggedIn ? "Bem-vindo de volta!" : "Por favor, faça o login";
+console.log(mensagem); // "Bem-vindo de volta!"
+
+const numero = -10;
+const resultado = numero >= 0 ? "positivo" : "negativo";
+console.log(resultado); // "negativo"
